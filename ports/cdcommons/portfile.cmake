5cb314132d651a3aefb0610d3beccfd5e186a22d
Damian Vicino <sdavtaker@users.noreply.github.com>
2026-06-02 12:05:36 -0400

    fix: use CDCOMMONS_BUILD_TESTS=OFF to disable tests in portfile

--- ports/cdcommons/portfile.cmake
+++ ports/cdcommons/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_TESTING=OFF
+        -DCDCOMMONS_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
